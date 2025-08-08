61032ba4d006e8010301a6faffe83a724255b12e
David Heinemeier Hansson <david@hey.com>
2025-08-08 18:00:30 +0200

    Set Cloudflare as the default DNS with Google as backup
    Solves a bunch of problems with shitty local DNS

--- install/config/config.sh
+++ install/config/config.sh
@@ -26,6 +26,9 @@ sudo gpgconf --launch dirmngr || true
 sudo sed -i 's|^\(auth\s\+required\s\+pam_faillock.so\)\s\+preauth.*$|\1 preauth silent deny=10 unlock_time=120|' "/etc/pam.d/system-auth"
 sudo sed -i 's|^\(auth\s\+\[default=die\]\s\+pam_faillock.so\)\s\+authfail.*$|\1 authfail deny=10 unlock_time=120|' "/etc/pam.d/system-auth"
 
+# Set Cloudflare as primary DNS (with Google as backup)
+sudo cp ~/.local/share/omarchy/default/systemd/resolved.conf /etc/systemd/
+
 # Set common git aliases
 git config --global alias.co checkout
 git config --global alias.br branch
