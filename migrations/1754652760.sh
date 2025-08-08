cbb9cc22e5a94a1c6c63e0c9bdbcd6a6ed473d58
David Heinemeier Hansson <david@hey.com>
2025-08-08 14:39:56 +0200

    Try to run this in a subshell instead

--- migrations/1754652760.sh
+++ migrations/1754652760.sh
@@ -1,4 +1,6 @@
 echo "Fix the expand icon margin in the Waybar style"
 
-~/.local/share/omarchy/bin/omarchy-refresh-config waybar/style.css
-~/.local/share/omarchy/bin/omarchy-restart-waybar || true
+(
+  ~/.local/share/omarchy/bin/omarchy-refresh-config waybar/style.css
+  ~/.local/share/omarchy/bin/omarchy-restart-waybar
+)
