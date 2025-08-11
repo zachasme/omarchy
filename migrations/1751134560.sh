7a1976a659f55c5a48c1c7dcef4b286f769e11d8
David Heinemeier Hansson <david@hey.com>
2025-08-11 16:06:10 +0200

    Improve description.

--- migrations/1751134560.sh
+++ migrations/1751134560.sh
@@ -6,6 +6,6 @@ export PATH="$OMARCHY_PATH/bin:$PATH"
 mkdir -p "$HOME/.config/uwsm/"
 omarchy-refresh-config uwsm/env
 
-echo -e "\n\e[31mOmarchy has a new PATH. You must immediately relaunch Hyprland or most Omarchy cmds won't work.\nPlease run Omarchy > Update again after the quick relaunch is complete.\e[0m"
+echo -e "\n\e[31mOmarchy bins have been added to PATH (and OMARCHY_PATH is now system-wide).\nYou must immediately relaunch Hyprland or most Omarchy cmds won't work.\nPlease run Omarchy > Update again after the quick relaunch is complete.\e[0m"
 echo
 gum confirm "Ready to relaunch Hyprland? (All applications will be closed)" && uwsm stop
