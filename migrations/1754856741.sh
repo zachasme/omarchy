30e7a323b8bedb24cd97d1c0aaf79f0e1c02157d
David Heinemeier Hansson <david@hey.com>
2025-08-11 16:44:58 +0200

    Fix rm of protected file

--- migrations/1754856741.sh
+++ migrations/1754856741.sh
@@ -1,5 +1,5 @@
 echo "Disable systemd-networkd-wait-online"
-rm /etc/systemd/system/systemd-networkd-wait-online.service.d/wait-for-only-one-interface.conf
 
+sudo rm -rf /etc/systemd/system/systemd-networkd-wait-online.service.d/wait-for-only-one-interface.conf
 sudo systemctl disable systemd-networkd-wait-online.service
 sudo systemctl mask systemd-networkd-wait-online.service
