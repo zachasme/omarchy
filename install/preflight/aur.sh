8cee2d3a8bdd3389a61630f5c8f4bd28fd498189
Bruce <hi+gh@bruceroettgers.eu>
2025-08-13 20:13:40 +0200

    Add disable chaotic aur env flag (#746)

--- install/preflight/aur.sh
+++ install/preflight/aur.sh
@@ -4,7 +4,7 @@
 sudo pacman -Sy --needed --noconfirm base-devel
 
 # Only add Chaotic-AUR if the architecture is x86_64 so ARM users can build the packages
-if [[ "$(uname -m)" == "x86_64" ]] && ! command -v yay &>/dev/null; then
+if [[ "$(uname -m)" == "x86_64" ]] && [ -z "$DISABLE_CHAOTIC" ] && ! command -v yay &>/dev/null; then
   # Try installing Chaotic-AUR keyring and mirrorlist
   if ! pacman-key --list-keys 3056513887B78AEB >/dev/null 2>&1 &&
     sudo pacman-key --recv-key 3056513887B78AEB &&
