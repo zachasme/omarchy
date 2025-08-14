bf49a86e98a77108e8f8e01a844b78dd97c5f5a2
David Heinemeier Hansson <david@hey.com>
2025-08-14 14:46:59 +0200

    Use omarchy-chromium-bin to give live theme changes

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -6,7 +6,7 @@ yay -S --noconfirm --needed \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
   slurp satty \
   mpv evince imv \
-  chromium
+  omarchy-chromium-bin
 
 # Add screen recorder based on GPU
 if lspci | grep -qi 'nvidia'; then
---
+++ migrations/1755164105.sh
@@ -0,0 +1,24 @@
+echo "Switch to Omarchy Chromium for synchronized theme switching"
+
+set_theme_colors() {
+  if [[ -f ~/.config/omarchy/current/theme/chromium.theme ]] && command -v chromium &>/dev/null; then
+    chromium --no-startup-window --set-theme-color="$(<~/.config/omarchy/current/theme/chromium.theme)"
+  else
+    # Use a default, neutral grey if theme doesn't have a color
+    chromium --no-startup-window --set-theme-color="28,32,39"
+  fi
+}
+
+if command -v chromium &>/dev/null; then
+  yay -Rns chromium
+  yay -Sy omarchy-chromium-bin
+
+  if pgrep -x chromium; then
+    if gum confirm "Chromium must be restarted. Ready?"; then
+      pkill -x chromium
+      set_theme_colors
+    fi
+  else
+    set_theme_colors
+  fi
+fi
