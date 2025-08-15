5a809db2a87c5cbd2e09e5c70769b26016ff4f3c
David Heinemeier Hansson <david@hey.com>
2025-08-15 09:28:24 +0200

    Only ask for the identification if it has not already been provided

--- install/config/identification.sh
+++ install/config/identification.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-export OMARCHY_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
-export OMARCHY_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+[ -z "$OMARCHY_USER_NAME" ] &&
+  export OMARCHY_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
+
+[ -z "$OMARCHY_USER_EMAIL" ] &&
+  export OMARCHY_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
