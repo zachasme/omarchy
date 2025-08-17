787296edab6a4a29742d31334cdcab87d706abae
David Heinemeier Hansson <david@hey.com>
2025-08-17 07:20:07 -0400

    Skip pinta until we can resolve the certificate issue on manual build
    cc @Torxed

--- install/apps/xtras.sh
+++ install/apps/xtras.sh
@@ -7,7 +7,8 @@ if [ -z "$OMARCHY_BARE" ]; then
     xournalpp localsend-bin
 
   # Packages known to be flaky or having key signing issues are run one-by-one
-  for pkg in pinta typora spotify zoom; do
+  # FIXME: Add pinta back to this list when we have dealt with certificate issue
+  for pkg in typora spotify zoom; do
     yay -S --noconfirm --needed "$pkg" ||
       echo -e "\e[31mFailed to install $pkg. Continuing without!\e[0m"
   done
