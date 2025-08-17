7ca72c598a309c064cfad678ac9be5b1c0c19799
David Heinemeier Hansson <david@hey.com>
2025-08-17 09:12:47 -0400

    Add dust to reveal used space
    Co-authored-by: @tobi

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -2,7 +2,7 @@
 
 yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
-  fd eza fzf ripgrep zoxide bat jq xmlstarlet \
+  fd eza fzf ripgrep zoxide bat dust jq xmlstarlet \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate bash-completion \
   alacritty
