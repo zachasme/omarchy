55330cce8853a0d7fa186f740fb8fd951841d70f
David Heinemeier Hansson <david@hey.com>
2025-08-17 09:59:44 -0400

    Add minimal starship prompt to terminal
    Co-authored-by: @tobi

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -4,5 +4,6 @@ yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
   fd eza fzf ripgrep zoxide bat dust jq xmlstarlet \
   wl-clipboard fastfetch btop \
-  man tldr less whois plocate bash-completion \
+  man tldr less whois plocate \
+  starship bash-completion \
   alacritty
