a1cd10c0a270a8fbc443db989db5dc1035fe56b2
David Heinemeier Hansson <david@hey.com>
2025-08-19 16:23:09 +0200

    Let it roll for testing

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
-set -e
+# set -e
 
 export PATH="$HOME/.local/share/omarchy/bin:$PATH"
 OMARCHY_INSTALL=~/.local/share/omarchy/install
