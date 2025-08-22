29ea8af5e140db1e71f482b4a1b8a83865cad022
David Heinemeier Hansson <david@hey.com>
2025-08-22 22:27:44 +0200

    Move Docker .desktop to TUI install

--- install/apps/tuis.sh
+++ install/apps/tuis.sh
@@ -2,4 +2,5 @@
 
 if [ -z "$OMARCHY_BARE" ]; then
   omarchy-tui-install "Disk Usage" "bash -c 'dust; read -n 1 -s'" float https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/qdirstat.png
+  omarchy-tui-install "Docker" "lazydocker" float https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/docker.png
 fi
