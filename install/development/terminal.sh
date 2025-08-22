bac5710707a376f528b37dc4a764ada7ced3cd79
David Heinemeier Hansson <david@hey.com>
2025-08-22 12:12:39 +0200

    Don't need wget, since curl is already part of arch

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yay -S --noconfirm --needed \
-  wget curl unzip inetutils impala \
+  unzip inetutils impala \
   fd eza fzf ripgrep zoxide bat dust jq xmlstarlet \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
