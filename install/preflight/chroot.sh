455fe890eccaa80508af4417ef2a3828b392d667
David Heinemeier Hansson <david@hey.com>
2025-08-23 20:23:08 +0200

    Just rely on the ISO autostart to set it instead of detecting it

--- install/preflight/chroot.sh
+++ install/preflight/chroot.sh
@@ -1,8 +1,4 @@
-# Chroot installations have some differences
-if ! cmp -s /proc/1/root/ / 2>/dev/null; then
-  export OMARCHY_CHROOT_INSTALL=1
-fi
-
+# Starting the installer with OMARCHY_CHROOT_INSTALL=1 will put it into chroot mode
 chrootable_systemctl_enable() {
   if [ -n "${OMARCHY_CHROOT_INSTALL:-}" ]; then
     sudo systemctl enable $1
