31d97335fc816eb73939957aedd152aaa9ff7f02
Taha <paprikataha@gmail.com>
2025-08-23 14:22:50 -0700

    Add Omarchy icon to waybar (#1017)
    * Add Omarchy icon to waybar
    
    * Add step to fonts.sh to install omarchy icon font
    
    * Style and fixup
    
    * Fixup installation of the omarchy font
    
    ---------
    
    Co-authored-by: David Heinemeier Hansson <david@hey.com>

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -2,6 +2,10 @@
 
 yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts noto-fonts-emoji
 
+mkdir -p ~/.local/share/fonts
+cp ~/.local/share/omarchy/config/omarchy.ttf ~/.local/share/fonts/
+fc-cache
+
 if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
 fi
