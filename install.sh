b4b8788c146829e47e5f11a3e2700567059ebce0
David Heinemeier Hansson <david@hey.com>
2025-08-23 22:36:38 +0200

    Use something safe in the arch-chroot

--- install.sh
+++ install.sh
@@ -118,8 +118,8 @@ show_subtext "You're done! So we're ready to reboot now..."
 if sudo test -f /etc/sudoers.d/99-omarchy-installer; then
   sudo rm -f /etc/sudoers.d/99-omarchy-installer &>/dev/null
   echo
-  read -n 1 -s -r -p "Remove the USB installer then press any key to reboot..."
-  echo
+  printf "Remove the USB installer then press Enter to reboot..."
+  read dummy
 fi
 
 sleep 2
