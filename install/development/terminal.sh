75ac6910d4c02c9d11dd3936ae90698bb2a4c9ef
David Heinemeier Hansson <david@hey.com>
2025-08-23 22:40:36 +0200

    Add back fastfetch which we lost when removing logging

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -3,7 +3,7 @@
 yay -S --noconfirm --needed \
   unzip inetutils impala \
   fd eza fzf ripgrep zoxide bat dust jq xmlstarlet \
-  wl-clipboard btop \
+  wl-clipboard fastfetch btop \
   man tldr less whois plocate \
   starship bash-completion \
   gum python-poetry-core python-terminaltexteffects \
