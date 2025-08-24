7bd1d5639990d9445563538998a36c4bfb5fb4c0
David Heinemeier Hansson <david@hey.com>
2025-08-24 17:14:13 +0200

    Remove AUR font

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts noto-fonts-emoji
+sudo pacman -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd noto-fonts noto-fonts-emoji
 
 mkdir -p ~/.local/share/fonts
 cp ~/.local/share/omarchy/config/omarchy.ttf ~/.local/share/fonts/
 fc-cache
 
 if [ -z "$OMARCHY_BARE" ]; then
-  yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
+  sudo pacman -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
 fi
