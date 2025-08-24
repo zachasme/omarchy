95139164a3e9b363f50296e544fd8f852c7ce04f
David Heinemeier Hansson <david@hey.com>
2025-08-24 07:26:24 +0200

    Skip all of powerprofilectl setup during CHROOT

--- install/config/power.sh
+++ install/config/power.sh
@@ -4,14 +4,16 @@
 # even if they're not running off a battery. So let's make sure that's changed to performance.
 yay -S --noconfirm python-gobject power-profiles-daemon
 
-# FIXME: Need a way to add the user system unit in CHROOT install
-if ls /sys/class/power_supply/BAT* &>/dev/null && [ -z "${OMARCHY_CHROOT_INSTALL:-}" ]; then
-  # This computer runs on a battery
-  powerprofilesctl set balanced || true
+# FIXME: Can't set powerprofilesctl during CHROOT, must do it afterwards
+if [ -z "${OMARCHY_CHROOT_INSTALL:-}" ]; then
+  if ls /sys/class/power_supply/BAT* &>/dev/null; then
+    # This computer runs on a battery
+    powerprofilesctl set balanced || true
 
-  # Enable battery monitoring timer for low battery notifications
-  systemctl --user enable --now omarchy-battery-monitor.timer
-else
-  # This computer runs on power outlet
-  powerprofilesctl set performance || true
+    # Enable battery monitoring timer for low battery notifications
+    systemctl --user enable --now omarchy-battery-monitor.timer
+  else
+    # This computer runs on power outlet
+    powerprofilesctl set performance || true
+  fi
 fi
