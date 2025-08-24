ee5f4eb925cc937bf036df7a62be4d3feabb81ea
David Heinemeier Hansson <david@hey.com>
2025-08-24 18:08:17 +0200

    I think these are the last spots

--- migrations/1753468218.sh
+++ migrations/1753468218.sh
@@ -1,4 +1,8 @@
 echo "Add Terminal Text Effects for rizzing Omarchy"
-if ! yay -Qe python-terminaltexteffects &>/dev/null; then
-  yay -S --noconfirm --needed python-terminaltexteffects
+if omarchy-pkg-aur-accessible; then
+  if ! yay -Q python-terminaltexteffects &>/dev/null; then
+    yay -S --noconfirm --needed python-terminaltexteffects
+  fi
+else
+  echo -e "\n\e[31mAUR is unavailable. Install python-terminaltexteffects via AUR manually later.\e[0m"
 fi
--- migrations/1754515289.sh
+++ migrations/1754515289.sh
@@ -1,4 +1,9 @@
 echo "Update and restart Walker to resolve stuck Omarchy menu"
 
-yay -Sy --noconfirm walker-bin
+if omarchy-pkg-aur-accessible; then
+  yay -Sy --noconfirm walker-bin
+else
+  echo -e "\n\e[31mAUR is unavailable. Update walker-bin via AUR manually later.\e[0m"
+fi
+
 omarchy-restart-walker
