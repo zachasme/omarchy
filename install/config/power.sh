d9424b2bffd353b61989ed65fcc743cd3c716e61
David Heinemeier Hansson <david@hey.com>
2025-08-24 07:09:45 +0200

    Document what's missing

--- install/config/power.sh
+++ install/config/power.sh
@@ -4,6 +4,7 @@
 # even if they're not running off a battery. So let's make sure that's changed to performance.
 yay -S --noconfirm python-gobject power-profiles-daemon
 
+# FIXME: Need a way to add the user system unit in CHROOT install
 if ls /sys/class/power_supply/BAT* &>/dev/null && [ -z "${OMARCHY_CHROOT_INSTALL:-}" ]; then
   # This computer runs on a battery
   powerprofilesctl set balanced || true
