69fe4ae9973a9bca255c36f131702a0bc266df6f
David Heinemeier Hansson <david@hey.com>
2025-08-24 11:00:30 +0200

    Remove screen recording status until it is stable (#1032)

--- migrations/1755930114.sh
+++ migrations/1755930114.sh
@@ -1,3 +1,3 @@
-echo "Add status indicators for screen recordings, nightlight, dnd, and idle lock to Waybar"
+echo "Add status indicators for nightlight, dnd, and idle lock to Waybar"
 echo
 gum confirm "Replace current Waybar config (backup will be made)?" && omarchy-refresh-waybar
