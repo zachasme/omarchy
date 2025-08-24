ca115b9061c57499bed2d690b9fe7671d4ab91d5
David Heinemeier Hansson <david@hey.com>
2025-08-24 09:12:51 +0200

    Can't install firewall in chroot, so move it into its own service for now

--- install/development/firewall.sh
+++ install/development/firewall.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-if ! command -v ufw &>/dev/null && -z "${OMARCHY_CHROOT_INSTALL:-}"; then
-  yay -S --noconfirm --needed ufw ufw-docker
-
-  # Allow nothing in, everything out
-  sudo ufw default deny incoming
-  sudo ufw default allow outgoing
-
-  # Allow ports for LocalSend
-  sudo ufw allow 53317/udp
-  sudo ufw allow 53317/tcp
-
-  # Allow SSH in
-  sudo ufw allow 22/tcp
-
-  # Allow Docker containers to use DNS on host
-  sudo ufw allow in proto udp from 172.16.0.0/12 to 172.17.0.1 port 53 comment 'allow-docker-dns'
-
-  # Turn on the firewall
-  sudo ufw --force enable
-
-  # Turn on Docker protections
-  sudo ufw-docker install
-  sudo ufw reload
+if -z "${OMARCHY_CHROOT_INSTALL:-}"; then
+  omarchy-install-firewall
 fi
