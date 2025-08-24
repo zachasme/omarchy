22769caa8cfd9ee2a92dfdca59cf78998f8ede45
David Heinemeier Hansson <david@hey.com>
2025-08-24 17:15:14 +0200

    Will install chromium by default and then bring our fork in through other means

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed \
+sudo pacman -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
   slurp satty wf-recorder \
   mpv evince imv \
-  omarchy-chromium-bin
+  chromium
