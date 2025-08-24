83b67d0d6be3f44d911700cbf6a9963543e25470
David Heinemeier Hansson <david@hey.com>
2025-08-24 19:32:26 +0200

    Now we have walker-bin in OPR

--- migrations/1754515289.sh
+++ migrations/1754515289.sh
@@ -1,9 +1,4 @@
 echo "Update and restart Walker to resolve stuck Omarchy menu"
 
-if omarchy-pkg-aur-accessible; then
-  yay -Sy --noconfirm walker-bin
-else
-  echo -e "\n\e[31mAUR is unavailable. Update walker-bin via AUR manually later.\e[0m"
-fi
-
+sudo pacman -Sy --noconfirm walker-bin
 omarchy-restart-walker
