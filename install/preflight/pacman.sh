115e9c5af8c48f9112a3ad68088ba57b8fd3939e
David Heinemeier Hansson <david@hey.com>
2025-08-24 19:28:02 +0200

    Add Omarchy repository first

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -7,3 +7,8 @@ sudo pacman -Sy --needed --noconfirm base-devel jq
 if ! grep -q "ILoveCandy" /etc/pacman.conf; then
   sudo sed -i '/^\[options\]/a Color\nILoveCandy\nVerbosePkgLists' /etc/pacman.conf
 fi
+
+# Add the Omarchy repository first
+if ! grep -q "omarchy" /etc/pacman.conf; then
+  sudo sed -i '/^\[core\]/i [omarchy]\nSigLevel = Optional TrustAll\nServer = https:\/\/pkgs.omarchy.org\/$arch\/\n' /etc/pacman.conf
+fi
