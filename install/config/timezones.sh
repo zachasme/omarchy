028c07d0163f67f448239b416fa7e5416d2b2aa7
David Heinemeier Hansson <david@hey.com>
2025-08-24 16:36:10 +0200

    Install tzupdate on demand

--- install/config/timezones.sh
+++ install/config/timezones.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if ! command -v tzupdate &>/dev/null; then
-  yay -S --noconfirm --needed tzupdate
-  sudo tee /etc/sudoers.d/omarchy-tzupdate >/dev/null <<EOF
+sudo tee /etc/sudoers.d/omarchy-tzupdate >/dev/null <<EOF
 %wheel ALL=(root) NOPASSWD: /usr/bin/tzupdate, /usr/bin/timedatectl
 EOF
-  sudo chmod 0440 /etc/sudoers.d/omarchy-tzupdate
-fi
+sudo chmod 0440 /etc/sudoers.d/omarchy-tzupdate
