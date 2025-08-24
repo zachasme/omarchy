9d355091b5ba8895cb5b12769955568465462ae1
David Heinemeier Hansson <david@hey.com>
2025-08-24 16:52:21 +0200

    No AUR dependency for wl-screenrec

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -4,13 +4,6 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
-  slurp satty \
+  slurp satty wf-recorder \
   mpv evince imv \
   omarchy-chromium-bin
-
-# Add screen recorder based on GPU
-if lspci | grep -Eqi 'nvidia|intel.*graphics'; then
-  yay -S --noconfirm --needed wf-recorder
-else
-  yay -S --noconfirm --needed wl-screenrec
-fi
