b38e864f0832edbcfb0f7e8c55d213ff021420a9
David Heinemeier Hansson <david@hey.com>
2025-08-25 11:41:36 +0200

    Fix path

--- migrations/1755208929.sh
+++ migrations/1755208929.sh
@@ -1,5 +1,4 @@
 echo "Change behaviour of XF86PowerOff button to show omarchy system menu insead of shutting down immediately"
 
-source $OMARCHY_PATH/install/config/ignore-power-button.sh
+source $OMARCHY_PATH/install/config/hardware/ignore-power-button.sh
 setsid systemd-inhibit --what=handle-power-key --why="Temporary disable power button before restart" sleep infinity &
-
