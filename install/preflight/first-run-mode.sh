fdaa126aeef54824a630b5152596a221a674f6b2
David Heinemeier Hansson <david@hey.com>
2025-08-25 10:57:29 +0200

    Fix sudo access permission

--- install/preflight/first-run-mode.sh
+++ install/preflight/first-run-mode.sh
@@ -6,8 +6,9 @@ touch ~/.local/state/omarchy/first-run.mode
 
 # Setup sudo-less access for first-run
 sudo tee /etc/sudoers.d/first-run >/dev/null <<EOF
+Cmnd_Alias FIRST_RUN_CLEANUP = /bin/rm -f /etc/sudoers.d/first-run
 $USER ALL=(ALL) NOPASSWD: /usr/bin/ufw
 $USER ALL=(ALL) NOPASSWD: /usr/bin/ufw-docker
-$USER ALL=(ALL) NOPASSWD: /bin/rm -f /etc/sudoers.d/first-run
+$USER ALL=(ALL) NOPASSWD: FIRST_RUN_CLEANUP
 EOF
 sudo chmod 440 /etc/sudoers.d/first-run
