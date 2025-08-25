d7fa8181fa59c844fc70570ea4babfe772fb6c55
David Heinemeier Hansson <david@hey.com>
2025-08-25 07:09:51 +0200

    Reinstall all the old AUR packages from OPR

--- migrations/1756060611.sh
+++ migrations/1756060611.sh
@@ -3,25 +3,13 @@ echo "Migrate AUR packages to official repos where possible"
 reinstall_package_opr() {
   if pacman -Q $1 >/dev/null; then
     sudo pacman -Rns --noconfirm $1
-    sudo pacman -S --noconfirm $1
+    sudo pacman -S --noconfirm ${2:-$1}
   fi
 }
 
-if command -v obsidian >/dev/null; then
-  sudo pacman -Rns --noconfirm obsidian-bin
-  sudo pacman -S --noconfirm obsidian
-fi
-
-if command -v localsend >/dev/null; then
-  sudo pacman -Rns --noconfirm localsend-bin
-  sudo pacman -S --noconfirm localsend
-fi
-
-if pacman -Q omarchy-chromium-bin >/dev/null; then
-  sudo pacman -Rns --noconfirm omarchy-chromium-bin
-  sudo pacman -S --noconfirm omarchy-chromium
-fi
-
+reinstall_package_opr obsidian-bin obsidian
+reinstall_package_opr localsend-bin localsend
+reinstall_package_opr omarchy-chromium-bin omarchy-chromium
 reinstall_package_opr walker-bin
 reinstall_package_opr wl-screenrec
 reinstall_package_opr python-terminaltexteffects
