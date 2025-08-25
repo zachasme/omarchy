6c656766f8b7859df7c7fbf10f8fd8506dcef2a0
David Heinemeier Hansson <david@hey.com>
2025-08-25 13:53:33 +0200

    Fix migration too

--- migrations/1756103168.sh
+++ migrations/1756103168.sh
@@ -1,6 +1,7 @@
 echo "Allow pacman -Sy without sudo to easier installs"
 
 sudo tee /etc/sudoers.d/repositories >/dev/null <<EOF
-$USER ALL=(ALL) NOPASSWD: /usr/binpacman -Sy
+$USER ALL=(ALL) NOPASSWD: /usr/bin/pacman -Sy
+$USER ALL=(ALL) NOPASSWD: /usr/bin/yay -Sy
 EOF
 sudo chmod 440 /etc/sudoers.d/repositories
