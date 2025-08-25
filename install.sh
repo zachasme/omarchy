705b6858dc7c4dcaf0fa9c46c367db1cc6b2d7cf
David Heinemeier Hansson <david@hey.com>
2025-08-25 09:31:07 +0200

    Order

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ OMARCHY_INSTALL=~/.local/share/omarchy/install
 # Install prerequisites
 source $OMARCHY_INSTALL/preflight/show-env.sh
 source $OMARCHY_INSTALL/preflight/trap-errors.sh
-source $OMARCHY_INSTALL/preflight/chroot.sh
 source $OMARCHY_INSTALL/preflight/guard.sh
+source $OMARCHY_INSTALL/preflight/chroot.sh
 source $OMARCHY_INSTALL/preflight/repositories.sh
 source $OMARCHY_INSTALL/preflight/migrations.sh
 source $OMARCHY_INSTALL/preflight/first-run-mode.sh
