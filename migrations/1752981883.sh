186912ca1dea17cef5cdda860a9a6b5c51234bf8
David Heinemeier Hansson <david@hey.com>
2025-08-25 09:39:24 +0200

    Last yays

--- migrations/1752981883.sh
+++ migrations/1752981883.sh
@@ -1,7 +1,7 @@
 echo "Replace wofi with walker as the default launcher"
 
 if ! command -v walker &>/dev/null; then
-  yay -Sy --noconfirm --needed walker-bin libqalculate
+  sudo pacman -Sy --noconfirm --needed walker-bin libqalculate
 
   sudo pacman -Rns --noconfirm wofi
   rm -rf ~/.config/wofi
