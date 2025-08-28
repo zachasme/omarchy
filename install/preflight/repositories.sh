a21ace2ee7bbb0f33c83d9777ecaa53f761f28c1
David Heinemeier Hansson <david@hey.com>
2025-08-28 21:50:49 +0200

    Drop Chaotic-AUR repository now that we have OPR (#1264)
    * Drop Chaotic AUR repo
    
    Now that we have the AUR packages we need on the OPR, we don't need this
    as a default.
    
    * Fix escape issue and rename so it will run again

--- install/preflight/repositories.sh
+++ install/preflight/repositories.sh
@@ -17,23 +17,5 @@ fi
 echo -e "Server = https://mirror.omarchy.org/\$repo/os/\$arch\nServer = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch\nServer = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch" |
   sudo tee /etc/pacman.d/mirrorlist >/dev/null
 
-# Only add Chaotic-AUR if the architecture is x86_64 so ARM users can build the packages
-if [[ "$(uname -m)" == "x86_64" ]] && [ -z "$DISABLE_CHAOTIC" ]; then
-  # Try installing Chaotic-AUR keyring and mirrorlist
-  if ! pacman-key --list-keys 3056513887B78AEB >/dev/null 2>&1 &&
-    sudo pacman-key --recv-key 3056513887B78AEB &&
-    sudo pacman-key --lsign-key 3056513887B78AEB &&
-    sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' &&
-    sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'; then
-
-    # Add Chaotic-AUR repo to pacman config
-    if ! grep -q "chaotic-aur" /etc/pacman.conf; then
-      echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf >/dev/null
-    fi
-  else
-    echo -e "Failed to install Chaotic-AUR, so won't include it in pacman config!"
-  fi
-fi
-
 # Refresh all repos
 sudo pacman -Syu --noconfirm
