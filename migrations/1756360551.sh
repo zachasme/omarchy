43ee2cf4e0f46c875cdc7350c73716a34edecb5d
David Heinemeier Hansson <david@hey.com>
2025-08-28 21:34:31 +0200

    Fix escaping bug as hotfix

--- migrations/1756360551.sh
+++ migrations/1756360551.sh
@@ -3,4 +3,4 @@ echo "Move Omarchy Package Repository after Arch core/extra/multilib for extra s
 sudo cp /etc/pacman.conf /etc/pacman.conf.bak
 sudo sed -i '/\[omarchy\]/,+2 d' /etc/pacman.conf
 sudo sed -i '/\[chaotic-aur\]/i\[omarchy]\nSigLevel = Optional TrustAll\nServer = https://pkgs.omarchy.org/$arch/\n' /etc/pacman.conf ||
-  sudo bash -c 'echo -e "\n[omarchy]\nSigLevel = Optional TrustAll\nServer = https://pkgs.omarchy.org/$arch/" >> /etc/pacman.conf'
+  sudo bash -c 'echo -e "\n[omarchy]\nSigLevel = Optional TrustAll\nServer = https://pkgs.omarchy.org/\$arch/" >> /etc/pacman.conf'
