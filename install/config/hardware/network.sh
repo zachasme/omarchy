4e7aa3386894ca8c2e753f8e94800b55887afba6
David Heinemeier Hansson <david@hey.com>
2025-08-28 07:05:03 +0200

    Don't need to start immediately

--- install/config/hardware/network.sh
+++ install/config/hardware/network.sh
@@ -4,7 +4,7 @@
 # This can happen if archinstall used ethernet
 if ! command -v iwctl &>/dev/null; then
   sudo pacman -S --noconfirm --needed iwd
-  chrootable_systemctl_enable iwd.service
+  sudo systemctl enable iwd.service
 fi
 
 # Prevent systemd-networkd-wait-online timeout on boot
