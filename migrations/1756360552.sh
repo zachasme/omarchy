defe63a91c7c8ea40582f1228b90761dcc6f1363
David Heinemeier Hansson <david@hey.com>
2025-08-29 19:44:07 +0200

    No trailing slash

--- migrations/1756360552.sh
+++ migrations/1756360552.sh
@@ -3,5 +3,5 @@ echo "Move Omarchy Package Repository after Arch core/extra/multilib and remove 
 sudo cp /etc/pacman.conf /etc/pacman.conf.bak
 sudo sed -i '/\[omarchy\]/,+2 d' /etc/pacman.conf
 sudo sed -i '/\[chaotic-aur\]/,+2 d' /etc/pacman.conf
-sudo bash -c 'echo -e "\n[omarchy]\nSigLevel = Optional TrustAll\nServer = https://pkgs.omarchy.org/\$arch/" >> /etc/pacman.conf'
+sudo bash -c 'echo -e "\n[omarchy]\nSigLevel = Optional TrustAll\nServer = https://pkgs.omarchy.org/\$arch" >> /etc/pacman.conf'
 sudo pacman -Syu --noconfirm
