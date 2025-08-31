a4c4b3570c6a7ba5fcbde1a1cbd2a956093c09c4
David Heinemeier Hansson <david@hey.com>
2025-08-31 15:07:36 +0200

    Ship our own pacman.conf

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -3,24 +3,9 @@
 # Install build tools
 sudo pacman -S --needed --noconfirm base-devel
 
-# Add fun and color and verbosity to the pacman installer
-if ! grep -q "ILoveCandy" /etc/pacman.conf; then
-  sudo sed -i '/^\[options\]/a Color\nILoveCandy\nVerbosePkgLists' /etc/pacman.conf
-fi
-
-# Add the Omarchy repository
-if ! grep -q "omarchy" /etc/pacman.conf; then
-  sudo tee -a /etc/pacman.conf >/dev/null <<'EOF'
-
-[omarchy]
-SigLevel = Optional TrustAll
-Server = https://pkgs.omarchy.org/$arch
-EOF
-fi
-
-# Set mirrors to global ones only
-omarchy-refresh-pacman-mirrorlist
+# Configure pacman
+sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
+sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
 
 # Refresh all repos
 sudo pacman -Syu --noconfirm
-
