fe20e89e0e90bc24cf670bbf82b9307ff31e8152
David Heinemeier Hansson <david@hey.com>
2025-08-31 13:08:40 +0200

    Set everything under the chromium guard

--- migrations/1755164105.sh
+++ migrations/1755164105.sh
@@ -1,15 +1,15 @@
 echo "Switch to Omarchy Chromium for synchronized theme switching"
 
-set_theme_colors() {
-  if [[ -f ~/.config/omarchy/current/theme/chromium.theme ]] && command -v chromium &>/dev/null; then
-    chromium --no-startup-window --set-theme-color="$(<~/.config/omarchy/current/theme/chromium.theme)"
-  else
-    # Use a default, neutral grey if theme doesn't have a color
-    chromium --no-startup-window --set-theme-color="28,32,39"
-  fi
-}
-
 if omarchy-cmd-present chromium; then
+  set_theme_colors() {
+    if [[ -f ~/.config/omarchy/current/theme/chromium.theme ]]; then
+      chromium --no-startup-window --set-theme-color="$(<~/.config/omarchy/current/theme/chromium.theme)"
+    else
+      # Use a default, neutral grey if theme doesn't have a color
+      chromium --no-startup-window --set-theme-color="28,32,39"
+    fi
+  }
+
   omarchy-pkg-drop chromium
   omarchy-pkg-add omarchy-chromium
 
