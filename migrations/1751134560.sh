91177a65463eb247496b408ea5469faf0c0f9c3e
David Heinemeier Hansson <david@hey.com>
2025-08-31 16:28:27 +0200

    Needed for pre-migrations setups

--- migrations/1751134560.sh
+++ migrations/1751134560.sh
@@ -9,6 +9,7 @@ omarchy-refresh-config uwsm/env
 echo -e "\n\e[31mOmarchy bins have been added to PATH (and OMARCHY_PATH is now system-wide).\nYou must immediately relaunch Hyprland or most Omarchy cmds won't work.\nPlease run Omarchy > Update again after the quick relaunch is complete.\e[0m"
 echo
 
+mkdir -p ~/.local/state/omarchy/migrations
 gum confirm "Ready to relaunch Hyprland? (All applications will be closed)" &&
   touch ~/.local/state/omarchy/migrations/1751134560.sh &&
   uwsm stop
