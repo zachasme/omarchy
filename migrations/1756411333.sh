a44ea9c23245ea288ca0b182df5234fa31751b31
David Heinemeier Hansson <david@hey.com>
2025-08-31 13:30:57 +0200

    Ensure new mirrorlist is added as early as possible

--- migrations/1756411333.sh
+++ migrations/1756411333.sh
@@ -1,7 +1,3 @@
 echo "Use new Omarchy mirror as default"
 
-sudo tee /etc/pacman.d/mirrorlist >/dev/null <<'EOF'
-Server = https://mirror.omarchy.org/$repo/os/$arch
-Server = https://mirror.rackspace.com/archlinux/$repo/os/$arch
-Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch
-EOF
+omarchy-refresh-pacman-mirrorlist
