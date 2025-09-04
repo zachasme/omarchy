ec428bc05f8a1ff81b60a38919958dfedbab2d7b
esteban_ba <ebaguirre@citykinect.com>
2025-09-04 16:10:51 -0700

    Create git config file under `~/.config`
    This will help to keep our git config alongside our other configuration
    files instead of in the home directory under `~/.gitconfig`

--- install/config/git.sh
+++ install/config/git.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Create git config file under .config if it doesn't exist
+# This helps to keep all our configs in one place
+if [[ ! -f "~/.config/git/config" ]]; then
+  mkdir -p "~/.config/git"
+  touch "~/.config/git/config"
+fi
+
 # Set common git aliases
 git config --global alias.co checkout
 git config --global alias.br branch
