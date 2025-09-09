6824ce0d0f523c6f95bc7790f4eb0a4f5ba472dd
Jarek <65668659+jardahrazdera@users.noreply.github.com>
2025-09-09 21:01:22 +0200

    Enable full mDNS resolution for .local domains (#1021)
    * Enable full mDNS resolution for .local domains
    
    * Add migration to enable mDNS resolution for existing users

--- install/config/hardware/printer.sh
+++ install/config/hardware/printer.sh
@@ -7,6 +7,9 @@ sudo mkdir -p /etc/systemd/resolved.conf.d
 echo -e "[Resolve]\nMulticastDNS=no" | sudo tee /etc/systemd/resolved.conf.d/10-disable-multicast.conf
 chrootable_systemctl_enable avahi-daemon.service
 
+# Enable mDNS resolution for .local domains
+sudo sed -i 's/^hosts:.*/hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns/' /etc/nsswitch.conf
+
 # Enable automatically adding remote printers
 if ! grep -q '^CreateRemotePrinters Yes' /etc/cups/cups-browsed.conf; then
   echo 'CreateRemotePrinters Yes' | sudo tee -a /etc/cups/cups-browsed.conf
