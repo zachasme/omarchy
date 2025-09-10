0bd34c5ad2811f73aa3875189f77352059ffa9ba
Zacharias Dyna Knudsen <z@chari.as>
2025-09-10 12:09:49 +0200

    Use better git defaults

--- install/config/git.sh
+++ install/config/git.sh
@@ -1,15 +1,3 @@
-# Ensure git settings live under ~/.config
-mkdir -p ~/.config/git
-touch ~/.config/git/config
-
-# Set common git aliases
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
-git config --global pull.rebase true
-git config --global init.defaultBranch master
-
 # Set identification from install inputs
 if [[ -n "${OMARCHY_USER_NAME//[[:space:]]/}" ]]; then
   git config --global user.name "$OMARCHY_USER_NAME"
---
+++ migrations/1757575120.sh
@@ -0,0 +1,5 @@
+echo "Add git defaults"
+
+omarchy-refresh-config git/config
+
+source $OMARCHY_PATH/install/config/git.sh
