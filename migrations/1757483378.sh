80767e8686d8f216c16aedd1792f0b7dd9b36b59
David Heinemeier Hansson <david@hey.com>
2025-09-10 07:58:27 +0200

    Add 6Ghz Wi-Fi + Intel graphics acceleration for existing installations

---
+++ migrations/1757483378.sh
@@ -0,0 +1,4 @@
+echo "6Ghz Wi-Fi + Intel graphics acceleration for existing installations"
+
+$OMARCHY_PATH/install/config/hardware/set-wireless-regdom.sh
+$OMARCHY_PATH/install/config/hardware/intel.sh
