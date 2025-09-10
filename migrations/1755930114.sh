a57060ee31e4b2e48ad47fa26454ff478b122fd6
Ofir Levitan <eifr0980@gmail.com>
2025-09-10 16:48:55 +0300

    feat(waybar): add recording indicator (#1561)
    * init
    
    * remove comment
    format
    
    * add pulse animation
    
    * change to signals
    
    * Move file to an internal location, match signal with what's being sent
    
    * Need to sleep a second to ensure that the recording has either started or stopped before we render config
    
    * Put it in a more prominent place
    
    * Use OMARCHY_PATH
    
    * Sharpen icons and add on-click to stop
    
    * Drop animation but reserve space
    
    * Lean entirely on waybar indicator rather than notification when starting
    
    * Use an explaining function instead of comments
    
    * Give time to ensure the process is dead before updating the indicator
    
    * No longer needed
    
    * Don't need to repeat the on-click
    
    * Don't need a full second
    
    * Naming
    
    * SIRGTMIN shouldn't be here
    
    * Don't need any of this that isn't seen or used
    
    * Explain the sleep
    
    * Mirror start/stop functions
    
    * Go all-in on explaining functions
    
    * Inline audio toggle
    
    * Better just to move the clock rather than have it be permanently offset
    
    * Fix all parameters and name them
    
    * Add migration
    
    * There is now a later omarchy-refresh-waybar offer
    
    ---------
    
    Co-authored-by: David Heinemeier Hansson <david@hey.com>

--- migrations/1755930114.sh
+++ migrations/1755930114.sh
@@ -3,6 +3,3 @@ echo "Add new Omarchy Menu icon to Waybar"
 mkdir -p ~/.local/share/fonts
 cp ~/.local/share/omarchy/config/omarchy.ttf ~/.local/share/fonts/
 fc-cache
-
-echo
-gum confirm "Replace current Waybar config (backup will be made)?" && omarchy-refresh-waybar
