5372393211373c1601ad257bc6f85d5cc237f36c
David Heinemeier Hansson <david@hey.com>
2025-09-13 17:40:19 +0200

    Remove interdependencies and ordering issues

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,6 +1,4 @@
 source $OMARCHY_INSTALL/helpers/chroot.sh
-source $OMARCHY_INSTALL/helpers/layout.sh
-source $OMARCHY_INSTALL/helpers/logo.sh
-source $OMARCHY_INSTALL/helpers/gum.sh
+source $OMARCHY_INSTALL/helpers/presentation.sh
 source $OMARCHY_INSTALL/helpers/errors.sh
 source $OMARCHY_INSTALL/helpers/logging.sh
