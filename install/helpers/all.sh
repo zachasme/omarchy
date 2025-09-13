6bf32a082e22a1b42fc639b9707c581ff2ed5729
David Heinemeier Hansson <david@hey.com>
2025-09-13 17:23:43 +0200

    Need layout defined first to use it in logo and gum

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,6 +1,6 @@
 source $OMARCHY_INSTALL/helpers/chroot.sh
+source $OMARCHY_INSTALL/helpers/layout.sh
 source $OMARCHY_INSTALL/helpers/logo.sh
 source $OMARCHY_INSTALL/helpers/gum.sh
 source $OMARCHY_INSTALL/helpers/errors.sh
 source $OMARCHY_INSTALL/helpers/logging.sh
-source $OMARCHY_INSTALL/helpers/layout.sh
