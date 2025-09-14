3e1e4669385e1f94dab019ff7dffe78d13a0f36d
David Heinemeier Hansson <david@hey.com>
2025-09-14 10:07:37 +0200

    Invite new installations to setup wifi

--- install/first-run/wifi.sh
+++ install/first-run/wifi.sh
@@ -1,3 +1,3 @@
 if ! ping -c5 -W1 1.1.1.1 >/dev/null 2>&1; then
-  setsid alacritty --class=Impala -e impala &
+  notify-send "󰖩   Click to Setup Wi-Fi" "Tab between sections, space selects, ? for help." -u critical -t 30000
 fi
