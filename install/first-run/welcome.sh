47bdbec9f13449857aa14cc7cf69f1284942c26f
David Heinemeier Hansson <david@hey.com>
2025-09-14 10:17:45 +0200

    Wording

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,1 +1,1 @@
-notify-send "👋   Welcome to Omarchy!" "Hit SUPER + ALT + SPACE to see Omarchy Menu." -u critical -t 30000
+notify-send "👋   Welcome to Omarchy!" "Hit SUPER + ALT + SPACE to start the Omarchy Menu." -u critical -t 30000
