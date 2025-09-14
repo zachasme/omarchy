55bf2457d41067b31e7c54f264ca300afdf66660
Bogdan Tanase <bftanase@gmail.com>
2025-09-14 16:51:24 +0200

    Feed the image folder to the Image Viewer so the next/previous image navigation works (#1444)
    * Feed the image folder to the Image Viewer so the next/previous image navigation works
    
    When double clicking an image in Nautilus, my expectation is that I can navigate to the other images in the folder with the arrow keys.
    
    This doesn't happen currently because the imv command receives only the filename as a parameter.
    
    However, if we use:
    
    `imv -n /myfolder/image.jpg /myfolder`
    
    works becasue we're opening  the folder starting at the image given by the -n parameter.
    
    * add migration for imv.desktop update
    
    * Simplify migration
    
    ---------
    
    Co-authored-by: David Heinemeier Hansson <david@hey.com>

---
+++ migrations/1757273064.sh
@@ -0,0 +1,4 @@
+echo "Allow Image Viewer to see all images in directory and use arrow keys to navigate"
+
+cp -f "$HOME/.local/share/omarchy/applications/imv.desktop" "$HOME/.local/share/applications/imv.desktop"
+
