13349180b60a29f791062c9cc24d99ddb6441b05
Alex R Teal <5629145+ATeal@users.noreply.github.com>
2025-09-14 10:20:08 -0400

    Better Zoom Webapp Support for meeting links (#1542)
    * Support for exec and mime types. Zoom integration
    
    In order to better support webapp zoom:
      - Added optional params for omarchy-webapp-install
        - exec, defaults to omarchy-launch-webapp
        - mimetypes, defaults blank
      - added zoom webapp launcher that parses meeting links and transforms
    them and calls launch webapp to join meeting links
      - migration to convert existing zoom installs to the new custom
    handler
      - updated the base installer to call new zoom handler and set
    mimetypes
    
    * default should be in the else
    
    * Add new line at end of file
    
    * Missed new line on migration
    
    * Updated conditionals to be a little more clean
    
    * This is a rare setup so let's just save it for the direct CLI
    
    * Use new bash conditionals
    
    * Rename to fit under the existing namespace of cmds
    
    * Fix after merge
    
    * Use new syntax and add missing segment comments
    
    * Cleanup a bit
    
    * Use local icon for zoom with migration
    
    * Fix regexp
    
    * Refer to raw local icon references
    
    ---------
    
    Co-authored-by: David Heinemeier Hansson <david@hey.com>

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,15 +1,13 @@
-ICON_DIR="$HOME/.local/share/applications/icons"
-
-omarchy-webapp-install "HEY" https://app.hey.com "$ICON_DIR/HEY.png"
-omarchy-webapp-install "Basecamp" https://launchpad.37signals.com "$ICON_DIR/Basecamp.png"
-omarchy-webapp-install "WhatsApp" https://web.whatsapp.com/ "$ICON_DIR/WhatsApp.png"
-omarchy-webapp-install "Google Photos" https://photos.google.com/ "$ICON_DIR/Google Photos.png"
-omarchy-webapp-install "Google Contacts" https://contacts.google.com/ "$ICON_DIR/Google Contacts.png"
-omarchy-webapp-install "Google Messages" https://messages.google.com/web/conversations "$ICON_DIR/Google Messages.png"
-omarchy-webapp-install "ChatGPT" https://chatgpt.com/ "$ICON_DIR/ChatGPT.png"
-omarchy-webapp-install "YouTube" https://youtube.com/ "$ICON_DIR/YouTube.png"
-omarchy-webapp-install "GitHub" https://github.com/ "$ICON_DIR/GitHub.png"
-omarchy-webapp-install "X" https://x.com/ "$ICON_DIR/X.png"
-omarchy-webapp-install "Figma" https://figma.com/ "$ICON_DIR/Figma.png"
-omarchy-webapp-install "Discord" https://discord.com/channels/@me "$ICON_DIR/Discord.png"
-omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home "$ICON_DIR/Zoom.png"
+omarchy-webapp-install "HEY" https://app.hey.com HEY.png
+omarchy-webapp-install "Basecamp" https://launchpad.37signals.com Basecamp.png
+omarchy-webapp-install "WhatsApp" https://web.whatsapp.com/ WhatsApp.png
+omarchy-webapp-install "Google Photos" https://photos.google.com/ "Google Photos.png"
+omarchy-webapp-install "Google Contacts" https://contacts.google.com/ "Google Contacts.png"
+omarchy-webapp-install "Google Messages" https://messages.google.com/web/conversations "Google Messages.png"
+omarchy-webapp-install "ChatGPT" https://chatgpt.com/ ChatGPT.png
+omarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png
+omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
+omarchy-webapp-install "X" https://x.com/ X.png
+omarchy-webapp-install "Figma" https://figma.com/ Figma.png
+omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
+omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "omarchy-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"
