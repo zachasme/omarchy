5582c51676a34c4a3d45c8edf8f357d720d2cb98
David Heinemeier Hansson <david@hey.com>
2025-09-15 11:36:07 +0200

    Add invitation to update the system to the latest

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,1 +1,2 @@
-notify-send "👋   Welcome to Omarchy" "You're in for a great computing adventure. Have fun!" -t 30000
+notify-send "    Update System" "When you have internet, click to update the system." -t 30000
+notify-send "👋  Welcome to Omarchy" "You're in for a great computing adventure. Have fun!" -t 30000
--- install/first-run/wifi.sh
+++ install/first-run/wifi.sh
@@ -1,3 +1,3 @@
 if ! ping -c3 -W1 1.1.1.1 >/dev/null 2>&1; then
-  notify-send "󰖩   Click to Setup Wi-Fi" "Tab between sections, space selects, ? for help." -u critical -t 30000
+  notify-send "󰖩    Click to Setup Wi-Fi" "Tab between sections, space selects, ? for help." -u critical -t 30000
 fi
