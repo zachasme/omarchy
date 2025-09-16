eec51b1fb519e93d80199190607c3e41242e2336
David Heinemeier Hansson <david@hey.com>
2025-09-16 21:46:06 +0200

    Add a migration to ensure that ghostty and kitty configs are available if someone switches over

---
+++ migrations/1758051607.sh
@@ -0,0 +1,9 @@
+echo "Copy configs for ghostty + kitty so they're available as alternative terminal options"
+
+if [[ ! -f ~/.config/ghostty/config ]]; then
+  cp -Rpf $OMARCHY_PATH/config/ghostty/config ~/.config/ghostty/config
+fi
+
+if [[ ! -f ~/.config/kitty/kitty.conf ]]; then
+  cp -Rpf $OMARCHY_PATH/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+fi
