c8fa96f38245e3f1f00bcae050def7feb0b94f39
David Heinemeier Hansson <david@hey.com>
2025-09-16 10:31:44 +0200

    Revert "Feed the image folder to the Image Viewer so the next/previous image navigation works (#1444)"
    This reverts commit 55bf2457d41067b31e7c54f264ca300afdf66660.

--- migrations/1757273064.sh
+++
@@ -1,4 +0,0 @@
-echo "Allow Image Viewer to see all images in directory and use arrow keys to navigate"
-
-cp -f "$HOME/.local/share/omarchy/applications/imv.desktop" "$HOME/.local/share/applications/imv.desktop"
-
