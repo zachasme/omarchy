bf0c2cc9a55fe7f8b6c22c0eb7514a70a3d4cde6
David Heinemeier Hansson <david@hey.com>
2025-09-16 15:14:29 +0200

    Allow custom editor, like we do custom terminal (#1697)

--- migrations/1758019332.sh
+++ migrations/1758019332.sh
@@ -1,10 +1,16 @@
-echo "Ensure $TERMINAL is set in uwsm env so entire system can rely on it"
+echo "Set \$TERMINAL and \$EDITOR in ~/.config/uwsm/default so entire system can rely on it"
 
-if ! grep -q "export TERMINAL" ~/.config/uwsm/env; then
-  omarchy-refresh-config uwsm/env
-  omarchy-state set relaunch-required
-fi
+# Set terminal and editor default in uwsm
+omarchy-refresh-config uwsm/default
+omarchy-refresh-config uwsm/env
+omarchy-state set relaunch-required
 
+# Ensure scrolltouchpad setting applies to all terminals
 if grep -q "scrolltouchpad 1.5, class:Alacritty" ~/.config/hypr/input.conf; then
   sed -i 's/windowrule = scrolltouchpad 1\.5, class:Alacritty/windowrule = scrolltouchpad 1.5, tag:terminal/' ~/.config/hypr/input.conf
+fi
+
+# Use default editor for keybinding
+if grep -q "bindd = SUPER, N, Neovim" ~/.config/hypr/bindings.conf; then
+  sed -i '/SUPER, N, Neovim, exec/ c\bindd = SUPER, N, Editor, exec, omarchy-launch-editor' ~/.config/hypr/bindings.conf
 fi
