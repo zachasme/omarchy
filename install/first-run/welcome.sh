e81994f1829a6d2aff6909e21ebb3bb3d464b1aa
David Heinemeier Hansson <david@hey.com>
2025-09-17 16:36:11 +0200

    Dont let the update system notification expire

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,2 +1,2 @@
-notify-send "    Update System" "When you have internet, click to update the system." -t 30000
+notify-send "    Update System" "When you have internet, click to update the system." -u critical
 notify-send "👋  Welcome to Omarchy" "You're in for a great computing adventure. Have fun!" -t 30000
