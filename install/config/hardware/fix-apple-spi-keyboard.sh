cd39bbf692d047e575a1be1e3bbe1131a5f6edd3
Ryan Hughes <ryan@heyoodle.com>
2025-09-28 14:52:24 -0400

    Fix conflict + rearrange

--- install/config/hardware/fix-apple-spi-keyboard.sh
+++ install/config/hardware/fix-apple-spi-keyboard.sh
@@ -1,14 +1,12 @@
 # Detect MacBook models that need SPI keyboard modules
-if [[ "$(cat /sys/class/dmi/id/product_name 2>/dev/null)" =~ MacBook[89],1|MacBook1[02],1|MacBookPro13,[123]|MacBookPro14,[123] ]]; then
+product_name="$(cat /sys/class/dmi/id/product_name 2>/dev/null)"
+if [[ "$product_name" =~ MacBook[89],1|MacBook1[02],1|MacBookPro13,[123]|MacBookPro14,[123] ]]; then
   echo "Detected MacBook with SPI keyboard"
 
   sudo pacman -S --noconfirm --needed macbook12-spi-driver-dkms
-  case "$(cat /sys/class/dmi/id/product_name 2>/dev/null)" in
-    MacBook8,1)
-      echo "MODULES=(applespi spi_pxa2xx_platform spi_pxa2xx_pci)" | sudo tee /etc/mkinitcpio.conf.d/macbook_spi_modules.conf >/dev/null
-      ;;
-    MacBook9,1|MacBook1[02],1|MacBookPro13,1|MacBookPro13,2|MacBookPro13,3|MacBookPro14,1|MacBookPro14,2|MacBookPro14,3)
-      echo "MODULES=(applespi intel_lpss_pci spi_pxa2xx_platform)" | sudo tee /etc/mkinitcpio.conf.d/macbook_spi_modules.conf >/dev/null
-      ;;
-  esac
+  if [[ "$product_name" == "MacBook8,1" ]]; then
+    echo "MODULES=(applespi spi_pxa2xx_platform spi_pxa2xx_pci)" | sudo tee /etc/mkinitcpio.conf.d/macbook_spi_modules.conf >/dev/null
+  else
+    echo "MODULES=(applespi intel_lpss_pci spi_pxa2xx_platform)" | sudo tee /etc/mkinitcpio.conf.d/macbook_spi_modules.conf >/dev/null
+  fi
 fi
