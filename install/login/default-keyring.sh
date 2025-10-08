d3f70a0c226292185be529e7d3af2fb0d61975a4
Ryan Hughes <ryan@heyoodle.com>
2025-10-08 19:21:26 -0400

    Make keyring dir

--- install/login/default-keyring.sh
+++ install/login/default-keyring.sh
@@ -2,6 +2,8 @@ KEYRING_DIR="$HOME/.local/share/keyrings"
 KEYRING_FILE="$KEYRING_DIR/Default_keyring.keyring"
 DEFAULT_FILE="$KEYRING_DIR/default"
 
+mkdir -p $KEYRING_DIR
+
 cat << EOF | tee "$KEYRING_FILE"
 [keyring]
 display-name=Default keyring
