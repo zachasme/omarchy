a20835f835d6b8766c4633520823320b554443fc
Ryan Hughes <ryan@heyoodle.com>
2025-10-12 19:25:19 -0400

    Add copy bind as well

--- migrations/1759895423.sh
+++ migrations/1759895423.sh
@@ -1,3 +1,3 @@
 echo "Make shift+insert paste from clipboard"
 sed -i '/keybind = f11=toggle_fullscreen/a keybind = shift+insert=paste_from_clipboard' ~/.config/ghostty/config
-sed -i 's/{ key = "F11", action = "ToggleFullscreen" }/{ key = "F11", action = "ToggleFullscreen" },\n{ key = "Insert", mods = "Shift", action = "Paste" }/' ~/.config/alacritty/alacritty.toml
+sed -i 's/{ key = "F11", action = "ToggleFullscreen" }/{ key = "F11", action = "ToggleFullscreen" },\n{ key = "Insert", mods = "Shift", action = "Paste" },\n{ key = "Insert", mods = "Control", action = "Copy" }/' ~/.config/alacritty/alacritty.toml
