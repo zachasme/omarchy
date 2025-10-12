7ef30f69b8ddb661c8210413858575bf9baccc36
David Heinemeier Hansson <david@hey.com>
2025-10-12 17:30:27 -0400

    Match the other themes

--- themes/flexoki-light/neovim.lua
+++ themes/flexoki-light/neovim.lua
@@ -1,15 +1,13 @@
 return {
-	{
-		"kepano/flexoki-neovim",
-		name = "flexoki",
-		config = function()
-			vim.api.nvim_set_option_value("background", "light", {})
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "flexoki-light",
-		},
-	},
+  {
+    "kepano/flexoki-neovim",
+    name = "flexoki",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "flexoki-light",
+    },
+  },
 }
