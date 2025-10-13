5b8ee60cc42e3701446397afdabfb63f5dfce3a5
sadd al <asaad5436@gmail.com>
2025-10-13 09:19:50 +0500

    fix: make bcm43xx driver script vendor agnostic

--- install/config/all.sh
+++ install/config/all.sh
@@ -26,7 +26,7 @@ run_logged $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
 run_logged $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 run_logged $OMARCHY_INSTALL/config/hardware/nvidia.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
-run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-bcm43xx.sh
+run_logged $OMARCHY_INSTALL/config/hardware/fix-bcm43xx.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-spi-keyboard.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-suspend-nvme.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-t2.sh
