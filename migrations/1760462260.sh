3d335a5278d1fb8085fe84114dc3c1b38745d009
David Heinemeier Hansson <david@hey.com>
2025-10-14 22:26:54 +0300

    Add migration for new hotkeys

---
+++ migrations/1760462260.sh
@@ -0,0 +1,20 @@
+echo -e "Offer new Omarchy hotkeys\n"
+
+cat <<EOF
+* Add SUPER + C / V for unified clipboard in both terminal and other apps
+* Add SUPER + SHIFT + V for clipboard manager
+* Move fullscreen from F11 to SUPER + F
+* Move app keys from SUPER + [LETTER] to SHIFT + SUPER + [LETTER]
+* Move toggling tiling/floating to SUPER + T
+EOF
+
+echo
+
+if gum confirm "Switch to new hotkeys?"; then
+  sed -i 's/SUPER,/SUPER SHIFT,/g' ~/.config/hypr/bindings.conf
+  sed -i 's/SUPER ALT,/SUPER SHIFT ALT,/g' ~/.config/hypr/bindings.conf
+  sed -i 's/SUPER CTRL,/SUPER SHIFT CTRL,/g' ~/.config/hypr/bindings.conf
+
+  sed -i 's|source = ~/.local/share/omarchy/default/hypr/bindings/tiling\.conf|source = ~/.local/share/omarchy/default/hypr/bindings/clipboard.conf\
+source = ~/.local/share/omarchy/default/hypr/bindings/tiling-v2.conf|' ~/.config/hypr/hyprland.conf
+fi
