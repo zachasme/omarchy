6247712205268306763633dd2a95ef120f3c6339
David Heinemeier Hansson <david@hey.com>
2025-10-15 19:09:08 +0300

    Keep terminal on SUPER + RETURN

--- migrations/1760462260.sh
+++ migrations/1760462260.sh
@@ -4,6 +4,7 @@ cat <<EOF
 * Add SUPER + C / V for unified clipboard in both terminal and other apps
 * Add SUPER + SHIFT + V for clipboard manager
 * Move fullscreen from F11 to SUPER + F
+* Keep terminal on SUPER + RETURN
 * Move app keys from SUPER + [LETTER] to SHIFT + SUPER + [LETTER]
 * Move toggling tiling/floating to SUPER + T
 EOF
@@ -13,6 +14,7 @@ echo
 if gum confirm "Switch to new hotkeys?"; then
   sed -i 's/SUPER SHIFT,/SUPER SHIFT ALT,/g' ~/.config/hypr/bindings.conf
   sed -i 's/SUPER,/SUPER SHIFT,/g' ~/.config/hypr/bindings.conf
+  sed -i 's/SUPER SHIFT, return/SUPER, RETURN/g' ~/.config/hypr/bindings.conf
   sed -i 's/SUPER ALT,/SUPER SHIFT ALT,/g' ~/.config/hypr/bindings.conf
   sed -i 's/SUPER CTRL,/SUPER SHIFT CTRL,/g' ~/.config/hypr/bindings.conf
   sed -i 's/SUPER SHIFT ALT, G, Google Messages/SUPER SHIFT CTRL, G, Google Messages/g' ~/.config/hypr/bindings.conf
