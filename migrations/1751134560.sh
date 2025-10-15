ac0ffa42dde750c7ca6b477891868499690ef822
David Heinemeier Hansson <david@hey.com>
2025-10-15 18:36:57 +0300

    Kick off the new migration from within itself

--- migrations/1751134560.sh
+++ migrations/1751134560.sh
@@ -9,10 +9,7 @@ export OMARCHY_PATH=$HOME/.local/share/omarchy
 export PATH=$OMARCHY_PATH/bin/:$PATH
 EOF
 
-echo -e "\n\e[31mOmarchy bins have been added to PATH (and OMARCHY_PATH is now system-wide).\nYou must immediately relaunch Hyprland or most Omarchy cmds won't work.\nPlease run Omarchy > Update again after the quick relaunch is complete.\e[0m"
-echo
-
 mkdir -p ~/.local/state/omarchy/migrations
-gum confirm "Ready to relaunch Hyprland? (All applications will be closed)" &&
-  touch ~/.local/state/omarchy/migrations/1751134560.sh &&
-  uwsm stop
+touch ~/.local/state/omarchy/migrations/1751134560.sh
+
+bash omarchy-update-perform
