c0dc0a0e472c3de54d5aa8d59d0f86aa27fba1bb
David Heinemeier Hansson <david@hey.com>
2025-10-15 18:25:05 +0300

    Revert "Update packages as the very first thing"
    This reverts commit 351d8ac93850181c3d18c1c062dc84946d637f88.

--- migrations/1751134561.sh
+++ migrations/1751134561.sh
@@ -1,10 +1,9 @@
 echo "Add Omarchy Package Repository"
 
-sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
+omarchy-refresh-pacman-mirrorlist
 
 if ! grep -q "omarchy" /etc/pacman.conf; then
   sudo sed -i '/^\[core\]/i [omarchy]\nSigLevel = Optional TrustAll\nServer = https:\/\/pkgs.omarchy.org\/$arch\n' /etc/pacman.conf
+  sudo systemctl restart systemd-timesyncd
+  sudo pacman -Syu --noconfirm
 fi
-
-sudo systemctl restart systemd-timesyncd
-sudo pacman -Syu --noconfirm
