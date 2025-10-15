06668cf20fc85c29b7460f5906161cac00a6663a
David Heinemeier Hansson <david@hey.com>
2025-10-15 17:48:15 +0300

    Reveal main keybindings on welcome

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,2 +1,2 @@
 notify-send "    Update System" "When you have internet, click to update the system." -u critical
-notify-send "👋  Welcome to Omarchy" "You're in for a great computing adventure. Have fun!" -t 30000
+notify-send "👋  Welcome to Omarchy" "Super + K to see keybindings.\nSuper + Space for application launcher.\nSuper + Alt + Space for Omarchy Menu." -t 30000
