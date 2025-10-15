bd52f8ae52840f2d166c24cade341ed579243021
Ryan Hughes <ryan@heyoodle.com>
2025-10-14 21:54:56 -0400

    Fix some missing bindings

--- migrations/1760462260.sh
+++ migrations/1760462260.sh
@@ -11,9 +11,11 @@ EOF
 echo
 
 if gum confirm "Switch to new hotkeys?"; then
+  sed -i 's/SUPER SHIFT,/SUPER SHIFT ALT,/g' ~/.config/hypr/bindings.conf
   sed -i 's/SUPER,/SUPER SHIFT,/g' ~/.config/hypr/bindings.conf
   sed -i 's/SUPER ALT,/SUPER SHIFT ALT,/g' ~/.config/hypr/bindings.conf
   sed -i 's/SUPER CTRL,/SUPER SHIFT CTRL,/g' ~/.config/hypr/bindings.conf
+  sed -i 's/SUPER SHIFT ALT, G, Google Messages/SUPER SHIFT CTRL, G, Google Messages/g' ~/.config/hypr/bindings.conf
 
   sed -i 's|source = ~/.local/share/omarchy/default/hypr/bindings/tiling\.conf|source = ~/.local/share/omarchy/default/hypr/bindings/clipboard.conf\
 source = ~/.local/share/omarchy/default/hypr/bindings/tiling-v2.conf|' ~/.config/hypr/hyprland.conf
