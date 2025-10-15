fecf83d07b81f0a96106d32d017ebec324dc5d86
David Heinemeier Hansson <david@hey.com>
2025-10-15 22:04:06 +0300

    Hookup keybindings notification to actual keybindings menu

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,2 +1,2 @@
 notify-send "    Update System" "When you have internet, click to update the system." -u critical
-notify-send "👋  Welcome to Omarchy" "Super + K to see keybindings.\nSuper + Space for application launcher.\nSuper + Alt + Space for Omarchy Menu." -t 30000
+notify-send "    Learn Keybindings" "Super + K for cheatsheet.\nSuper + Space for application launcher.\nSuper + Alt + Space for Omarchy Menu." -u critical
