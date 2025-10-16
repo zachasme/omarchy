6b1b73b87c98e9b96dd456db74fcc58062c6db7f
David Heinemeier Hansson <david@hey.com>
2025-10-16 13:59:08 +0300

    Use uwsm-app instead of uwsm app consistently

--- migrations/1753138691.sh
+++ migrations/1753138691.sh
@@ -2,5 +2,5 @@ echo "Install swayOSD to show volume status"
 
 if omarchy-cmd-missing swayosd-server; then
   omarchy-pkg-add swayosd
-  setsid uwsm app -- swayosd-server &>/dev/null &
+  setsid uwsm-app -- swayosd-server &>/dev/null &
 fi
