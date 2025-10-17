5a10222dc9743e89ebf6911061e160d9b63f7b02
David Heinemeier Hansson <david@hey.com>
2025-10-17 23:45:38 +0200

    No longer needed
    We are moving all the bindings together

--- migrations/1759946485.sh
+++
@@ -1,2 +0,0 @@
-echo "Move calendar to SUPER + SHIFT + C"
-sed -i 's/bindd = SUPER, C, Calendar, exec, omarchy-launch-webapp "https:\/\/app\.hey\.com\/calendar\/weeks\/"/bindd = SUPER SHIFT, C, Calendar, exec, omarchy-launch-webapp "https:\/\/app.hey.com\/calendar\/weeks\/"/' ~/.config/hypr/*.conf
