be24e38ea2c742510bdddd7d1626a76e61b75c47
Ryan Hughes <ryan@heyoodle.com>
2025-10-24 02:29:21 -0400

    Add wayfreeze

--- migrations/1761274980.sh
+++ migrations/1761274980.sh
@@ -1,6 +1,11 @@
-echo "Migrate localsend-bin to localsend package"
+echo "Migrate to correct packages for localsend and wayfreeze"
 
 if omarchy-pkg-present localsend-bin; then
   omarchy-pkg-drop localsend-bin
   omarchy-pkg-add localsend
+fi
+
+if omarchy-pkg-present wayfreeze-git; then
+  omarchy-pkg-drop wayfreeze-git
+  omarchy-pkg-add wayfreeze
 fi
