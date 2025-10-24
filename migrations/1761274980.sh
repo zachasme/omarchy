d2efdcede4a1ba2e088abb04e608d3eaadf1f504
Ryan Hughes <ryan@heyoodle.com>
2025-10-24 02:33:29 -0400

    Revert "Add wayfreeze"
    This reverts commit be24e38ea2c742510bdddd7d1626a76e61b75c47.

--- migrations/1761274980.sh
+++ migrations/1761274980.sh
@@ -1,11 +1,6 @@
-echo "Migrate to correct packages for localsend and wayfreeze"
+echo "Migrate localsend-bin to localsend package"
 
 if omarchy-pkg-present localsend-bin; then
   omarchy-pkg-drop localsend-bin
   omarchy-pkg-add localsend
-fi
-
-if omarchy-pkg-present wayfreeze-git; then
-  omarchy-pkg-drop wayfreeze-git
-  omarchy-pkg-add wayfreeze
 fi
