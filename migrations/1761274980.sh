cff15363c9ad19855533bacc9b8385b227697e09
Ryan Hughes <ryan@heyoodle.com>
2025-10-24 02:37:17 -0400

    Add asdcontrol

--- migrations/1761274980.sh
+++ migrations/1761274980.sh
@@ -1,6 +1,11 @@
-echo "Migrate localsend-bin to localsend package"
+echo "Migrate to proper packages for localsend and asdcontrol"
 
 if omarchy-pkg-present localsend-bin; then
   omarchy-pkg-drop localsend-bin
   omarchy-pkg-add localsend
+fi
+
+if omarchy-pkg-present asdcontrol-git; then
+  omarchy-pkg-drop asdcontrol-git
+  omarchy-pkg-add asdcontrol
 fi
